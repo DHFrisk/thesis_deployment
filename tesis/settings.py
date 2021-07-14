76653b9659122ace31b7ebd7de0a0a3ca5b121f2
ricboarg@gmail.com <ricboarg@gmail.com>
2021-07-13 20:26:36 -0600

    trying3

--- tesis/settings.py
+++ tesis/settings.py
@@ -166,9 +166,9 @@ STATIC_URL = '/static/'
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_DIRS= [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS= [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
