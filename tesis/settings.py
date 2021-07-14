57dd20bae4d37d8db5c7001cb9f8e33cd4d627d6
ricboarg@gmail.com <ricboarg@gmail.com>
2021-07-13 20:25:10 -0600

    trying3

--- tesis/settings.py
+++ tesis/settings.py
@@ -163,6 +163,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'static'
+
 STATICFILES_DIRS= [
     os.path.join(BASE_DIR, 'static')
 ]
